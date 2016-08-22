1265b26cd4cb212c8be4e57ac79e7fedac856adb
intuito <intuito@users.noreply.github.com>
2016-08-22 14:21:53 +0500

    Add comments
    Добавление комментариев, поясняющих назначение всех файл-скриптов и файл-функций

--- cilinder2decart.m
+++ cilinder2decart.m
@@ -1,3 +1,8 @@
+%
+% Функция преобразвания цилиндрических координат в декартовы прямоугольные
+% [x,y,z] = cilnder2dec(Ro,Phi,Z)
+%
+
 function [x, y, z] = cilinder2decart(Ro,Phi,Z)
     x = Ro .* cos(Phi);
     y = Ro .* sin(Phi);
