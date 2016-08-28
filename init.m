ad19ca24f515d517ba8a02de1779d5065e0443ec
intuito <ivan_@ip-192-168-72-1.is74.loc>
2016-08-28 11:25:42 +0600

    Translate comments in init file

--- init.m
+++ init.m
@@ -1,56 +1,60 @@
 %
-% Программа инизиализации системы, записывает структуру параметров системы и
-% сами параметры в файл с данными 
-clc; clear all; tic;
-%% Блок парметров
-% Начальные условия
+% This program will calculate program and desired trajectory
+% of control plant.
+% Then, it saves in <info.mat> file in <trj> structure with <par>
+% structure, which is contained all parameters of system,
+% such as initial conditions, limit of control actions, system parameters
+% ($/alpha, /beta, V$) and time and step of simulation.
+clc; clearvars; tic;
+%% Par structure creating
+% initial conditions
 par.R0 = 1; 
 par.Phi0 = 0;
 par.Z0 = 10;
-% Ограничение на управление
+% limit of control actions
 par.Us = 5 * pi / 180;
-% Параметры системы
+% System parameters
 par.alpha = 10 * pi / 180;
 par.beta = -1 * pi / 180;
 par.V = 1;
-% Время моделирования
-par.t12 = 130; % [с]
-% Временной шаг
-par.T0 = 0.001; %[c]
+% time of simulation
+par.t12 = 5; % [s]
+% step of simulation
+par.T0 = 0.001; %[s]
 
-%% Блок выделения памяти и инициализации начальных условий и всмогательных столбцов
-% Программные:
-trj.Pr.Ro = zeros(ceil(par.t12/par.T0)+1,1);% Радиус, м
-trj.Pr.Phi = zeros(ceil(par.t12/par.T0)+1,1);% Угол, рад
-trj.Pr.Z = zeros(ceil(par.t12/par.T0)+1,1);% Высота, м
-%Желаемые
-trj.Des.Ro = zeros(ceil(par.t12/par.T0)+1,1);% Радиус, м
-trj.Des.Phi = zeros(ceil(par.t12/par.T0)+1,1);% Угол, рад
-trj.Des.Z = zeros(ceil(par.t12/par.T0)+1,1);% Высота, м
+%% Memory allocation to trajectory structure, and its initialization
+% Program
+trj.Pr.Ro = zeros(ceil(par.t12/par.T0)+1,1);% Radius, m
+trj.Pr.Phi = zeros(ceil(par.t12/par.T0)+1,1);% Polar angle, rad
+trj.Pr.Z = zeros(ceil(par.t12/par.T0)+1,1);% Height, m
+% Desired
+trj.Des.Ro = zeros(ceil(par.t12/par.T0)+1,1);% Radius, m
+trj.Des.Phi = zeros(ceil(par.t12/par.T0)+1,1);%  Polar angle, rad
+trj.Des.Z = zeros(ceil(par.t12/par.T0)+1,1);% Height, m
 
 trj.t = 0:par.T0:par.t12;%Сетка времени
 trj.nu = 0.05 * sin(20 * pi * trj.t);
 trj.eta = -0.05 * cos(20 * pi * trj.t);
 
-trj.Pr.Ro(1) = par.Z0;
+trj.Pr.Ro(1) = par.R0;
 trj.Pr.Phi(1) = par.Phi0;
 trj.Pr.Z(1) = par.Z0;
 
-%% Вычисление программной траектории
+%% Integrate program trajectory 
 for i = 2:(ceil(par.t12/par.T0)+1)
     trj.Pr.Ro(i) = trj.Pr.Ro(i-1) + par.V * cos(par.alpha) * sin(par.beta) * par.T0;
     trj.Pr.Phi(i) = trj.Pr.Phi(i-1) + par.V / (trj.Pr.Ro(i)+trj.nu(i)) * cos(par.alpha) * cos(par.beta) * par.T0;
     trj.Pr.Z(i) = trj.Pr.Z(i-1) - par.V * sin(par.alpha) * par.T0;
 end;
-%% Вычисление желаемой траектории
+%% Calculate desired trajectory
 trj.Des.Ro = trj.Pr.Ro + trj.nu';
 trj.Des.Phi = trj.Pr.Phi;
 trj.Des.Z = trj.Pr.Z + trj.eta';
-%% Преобразовние к декартовым координатам и построение графиков
+%% coordinate tranformation and drawing plot
 [trj.Pr.x, trj.Pr.y, trj.Pr.z] = cilinder2decart(trj.Pr.Ro,trj.Pr.Phi,trj.Pr.Z);
 [trj.Des.x, trj.Des.y, trj.Des.z] = cilinder2decart(trj.Des.Ro,trj.Des.Phi,trj.Des.Z);
-plot3(trj.Pr.x, trj.Pr.y, trj.Pr.z); hold on; grid on;
-plot3(trj.Des.x, trj.Des.y, trj.Des.z); 
+plot3(trj.Pr.x, trj.Pr.y, trj.Pr.z,'red'); hold on; grid on;
+plot3(trj.Des.x, trj.Des.y, trj.Des.z,'blue');
 
 save info.mat trj par
 toc;
--- main.m
+++ main.m
@@ -1,52 +1,96 @@
 % Основная программа, интегрирующая систему с параметрами из файла info.mat
-clear all; tic;
+clearvars; tic;
 load info.mat % Загрузка файла с параметрами
 %% Выделение памяти
 % Координаты состояния
 trj.Ro = zeros(ceil(par.t12/par.T0)+1,1);
 trj.Phi = zeros(ceil(par.t12/par.T0)+1,1);
 trj.Z = zeros(ceil(par.t12/par.T0)+1,1);
+% Производные координат состояния
+trj.Der.Ro = zeros(ceil(par.t12/par.T0)+1,1);
+trj.Der.Phi = zeros(ceil(par.t12/par.T0)+1,1);
+trj.Der.Z = zeros(ceil(par.t12/par.T0)+1,1);
 % Вариации координат состояния
 trj.v.Ro = zeros(ceil(par.t12/par.T0)+1,1);
 trj.v.Phi = zeros(ceil(par.t12/par.T0)+1,1);
 trj.v.Z = zeros(ceil(par.t12/par.T0)+1,1);
 %Управление
 u1 = zeros(ceil(par.t12/par.T0)+1,1);
-u2 = u1;
+u2 = zeros(ceil(par.t12/par.T0)+1,1);
 %% Начальные условия
-trj.Ro(1) = par.Z0;
+trj.Ro(1) = par.R0;
 trj.Phi(1) = par.Phi0;
 trj.Z(1) = par.Z0;
 
-trj.v.Ro(1) = trj.Des.Ro(1) - trj.Ro(1);
-trj.v.Phi(1) = trj.Des.Phi(1) - trj.Phi(1);
-trj.v.Z(1) = trj.Des.Z(1) - trj.Z(1);
+trj.Der.Ro(1) = par.V * cos(par.alpha + u1(1)) * sin(par.beta + u2(1));
+trj.Der.Phi(1) = par.V / trj.Ro(1) * cos(par.alpha + u1(1)) * cos(par.beta + u2(1));
+trj.Der.Z(1) = - par.V * sin(par.alpha+u1(1));
+
+trj.v.Ro(1) = trj.Pr.Ro(1) - trj.Ro(1);
+trj.v.Phi(1) = trj.Pr.Phi(1) - trj.Phi(1);
+trj.v.Z(1) = trj.Pr.Z(1) - trj.Z(1);
 %% Интегрирование методом Адамса
 for i = 2:(ceil(par.t12/par.T0)+1)
-   trj.v.Ro(i) = trj.Des.Ro(i-1) - trj.Ro(i-1);
-   trj.v.Phi(i) = trj.Des.Phi(i-1) - trj.Phi(i-1);
-   trj.v.Z(i) = trj.Des.Z(i-1) - trj.Z(i-1);
+   trj.v.Ro(i) = trj.Pr.Ro(i-1) - trj.Ro(i-1);
+   trj.v.Phi(i) = trj.Pr.Phi(i-1) - trj.Phi(i-1);
+   trj.v.Z(i) = trj.Pr.Z(i-1) - trj.Z(i-1);
    
-   u1(i) = -3*trj.v.Z(i);
-   u2(i) = trj.v.Z(i);
-   trj.Ro(i) = trj.Ro(i-1) + par.V * cos(par.alpha + u1(i)) * sin(par.beta + u2(i)) * par.T0;
-   trj.Phi(i) = trj.Phi(i) + par.V / trj.Ro(i) * cos(par.alpha + u1(i)) * cos(par.beta + u2(i)) * par.T0;
-   trj.Z(i) = trj.Z(i-1) - par.V * sin(par.alpha+u1(i)); 
+   u1(i) = 0;
+   u2(i) = 0;
+   
+   trj.Der.Ro(i) = par.V * cos(par.alpha + u1(i)) * sin(par.beta + u2(i));
+   trj.Ro(i) = trj.Ro(i-1) + trj.Der.Ro(i) * par.T0;
+   trj.Der.Phi(i) = par.V / trj.Ro(i) * cos(par.alpha + u1(i)) * cos(par.beta + u2(i));
+   trj.Phi(i) = trj.Phi(i-1) + trj.Der.Phi(i) * par.T0;
+   trj.Der.Z(i) = - par.V * sin(par.alpha+u1(i));
+   trj.Z(i) = trj.Z(i-1) + trj.Der.Z(i) * par.T0; 
 end;
-% График фатической и желаемой траекторий
+% График фактической и программной траекторий
 [trj.x, trj.y, trj.z] = cilinder2decart(trj.Ro,trj.Phi,trj.Z);
-plot3(trj.x, trj.y, trj.z); hold on; grid on;
-plot3(trj.Des.x, trj.Des.y, trj.Des.z);
-% Вариации координат состояния
+plot3(trj.x, trj.y, trj.z,'green'); hold on; grid on;
+plot3(trj.Des.x, trj.Des.y, trj.Des.z,'red');
+plot3(trj.Pr.x,trj.Pr.y,trj.Pr.z,'blue');
+legend('Траектория','Желаемая','Программная');
+% Вариации координат состояния относительно программной и желаемой траектории
 figure;
-%subplot(3,1,1);
-plot(trj.t,trj.v.Ro); grid on;
-xlabel('t,c'); ylabel('/Rho,m');
+subplot(3,1,1);
+plot(trj.t,trj.v.Ro); grid on; hold on;
+plot(trj.t,trj.Des.Ro-trj.Ro);
+xlabel('t,c'); ylabel('\rho,m');
+title('Вариации координат состояния');
 
 subplot(3,1,2);
 plot(trj.t,trj.v.Phi.*180./pi); grid on;
 xlabel('t,c'); ylabel('\Phi, deg');
  
 subplot(3,1,3);
-% plot(trj.t,trj.v.Z); grid on;
-% xlabel('t,c'); ylabel('Z, м');
+plot(trj.t,trj.v.Z); grid on;
+xlabel('t,c'); ylabel('Z, м');
+
+figure;
+subplot(2,3,1);
+plot(trj.t,trj.Ro,'green'); grid on; hold on;
+plot(trj.t,trj.Des.Ro,'red');
+xlabel('t,c'); ylabel('\rho,m');
+
+subplot(2,3,2);
+plot(trj.t,trj.Phi.*180./pi,'green'); grid on;
+xlabel('t,c'); ylabel('\Phi, deg');
+title('Координаты состояния и их производные')
+ 
+subplot(2,3,3);
+plot(trj.t,trj.Z,'green'); grid on;
+xlabel('t,c'); ylabel('Z,м');
+
+subplot(2,3,4);
+plot(trj.t,trj.Der.Ro,'green'); grid on; hold on;
+plot(trj.t,trj.Des.Ro,'red');
+xlabel('t,c'); ylabel('d\rho/dt,m/c');
+
+subplot(2,3,5);
+plot(trj.t,trj.Der.Phi.*180./pi,'green'); grid on;
+xlabel('t,c'); ylabel('d\phi/dt, deg/c');
+ 
+subplot(2,3,6);
+plot(trj.t,trj.Der.Z,'green'); grid on;
+xlabel('t,c'); ylabel('dZ/dt,м/c');
